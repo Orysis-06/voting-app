2dea894c7964211873f636cb1c058883c17c20bd
unknown <moonosongiso386@gmail.com>
2025-10-14 14:54:51 +0200

    style: add general styling

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import image from "../assets/404.gif";
+
 function ErrorPage(){
+    const navigate = useNavigate();
+
+    // redirect user to the previous page after 6 seconds
+    useEffect(() => {
+        setTimeout(() => {
+            navigate(-1);
+        }, 6000)
+    })
+
     return(
-        <div>Errorpage</div>
+        <section className="error-page">
+            <div className="error-page-container">
+                <img src={image} alt="Page not found"/>
+                <h1>404</h1>
+                <p>This page does not exist. You will be redirected to the previous page shortly.</p>
+            </div>
+        </section>
     )
 }
 
